251c87eec50f9179b81387d57a52f2895b1acaff
paulmthompson <pmt10@case.edu>
2015-11-13 19:14:35 -0500

    added constants file and amplifier types

---
+++ src/constants.jl
@@ -0,0 +1,82 @@
+#=
+Constants
+=#
+
+#Constant parameters
+
+const USB_BUFFER_SIZE = 2400000
+const RHYTHM_BOARD_ID = 500
+const MAX_NUM_DATA_STREAMS = 8
+const FIFO_CAPACITY_WORDS = 67108864
+
+const WireInResetRun = 0x00
+const WireInMaxTimeStepLsb = 0x01
+const WireInMaxTimeStepMsb = 0x02
+const WireInDataFreqPll = 0x03
+const WireInMisoDelay = 0x04
+const WireInCmdRamAddr = 0x05
+const WireInCmdRamBank = 0x06
+const WireInCmdRamData = 0x07
+const WireInAuxCmdBank1 = 0x08
+const WireInAuxCmdBank2 = 0x09
+const WireInAuxCmdBank3 = 0x0a
+const WireInAuxCmdLength1 = 0x0b
+const WireInAuxCmdLength2 = 0x0c
+const WireInAuxCmdLength3 = 0x0d
+const WireInAuxCmdLoop1 = 0x0e
+const WireInAuxCmdLoop2 = 0x0f
+const WireInAuxCmdLoop3 = 0x10
+const WireInLedDisplay = 0x11
+const WireInDataStreamSel1234 = 0x12
+const WireInDataStreamSel5678 = 0x13
+const WireInDataStreamEn = 0x14
+const WireInTtlOut = 0x15
+const WireInDacSource1 = 0x16
+const WireInDacSource2 = 0x17
+const WireInDacSource3 = 0x18
+const WireInDacSource4 = 0x19
+const WireInDacSource5 = 0x1a
+const WireInDacSource6 = 0x1b
+const WireInDacSource7 = 0x1c
+const WireInDacSource8 = 0x1d
+const WireInDacManual = 0x1e
+const WireInMultiUse = 0x1f
+
+const TrigInDcmProg = 0x40
+const TrigInSpiStart = 0x41
+const TrigInRamWrite = 0x42
+const TrigInDacThresh = 0x43
+const TrigInDacHpf = 0x44
+const TrigInExtFastSettle = 0x45
+const TrigInExtDigOut = 0x46
+
+const WireOutNumWordsLsb = 0x20
+const WireOutNumWordsMsb = 0x21
+const WireOutSpiRunning = 0x22
+const WireOutTtlIn = 0x23
+const WireOutDataClkLocked = 0x24
+const WireOutBoardMode = 0x25
+const WireOutBoardId = 0x3e
+const WireOutBoardVersion = 0x3f
+
+const PipeOutData = 0xa0
+
+#For 32 channel amps
+const PortA1 = 0
+const PortA2 = 1
+const PortB1 = 2
+const PortB2 = 3
+const PortC1 = 4
+const PortC2 = 5
+const PortD1 = 6
+const PortD2 = 7
+
+#For 64 channel amps
+const PortA1Ddr = 8
+const PortA2Ddr = 9
+const PortB1Ddr = 10
+const PortB2Ddr = 11
+const PortC1Ddr = 12
+const PortC2Ddr = 13
+const PortD1Ddr = 14
+const PortD2Ddr = 15
--- src/rhd2000evalboard.jl
+++ src/rhd2000evalboard.jl
@@ -1,8 +1,16 @@
 
 
-#Constant parameters
+abstract Amp
 
-type RHD2000
+type RHD2164 <: Amp
+    port::ASCIIString
+end
+
+type RHD2132 <: Amp
+    port::ASCIIString
+end
+
+type RHD2000{T<:Amp}
     lib::ASCIIString
     bit::ASCIIString
     sampleRate::Int64
@@ -10,12 +18,11 @@ type RHD2000
     dataStreamEnabled::Array{Int64,2}
     usbBuffer::Array{Uint8,1}
     numWords::Int64
-    numBytesPerBlock
+    numBytesPerBlock::Int64
+    amps::Array{T,1}
 end
 
-function RHD2000(lib::ASCIIString,bit::ASCIIString)
-    mylib="/home/nicolelislab/Intan.jl/libokFrontPanel.so"
-    myfile="/home/nicolelislab/Intan.jl/main.bit"
+function RHD2000(sampleRate; lib="../lib/libokFrontPanel.so",bit="../lib/main.bit")
 
     sampleRate=30000
     numDataStreams=0
@@ -32,84 +39,7 @@ function RHD2000(lib::ASCIIString,bit::ASCIIString)
     RHD2000(lib,bit,sampleRate,numDataStreams,dataStreamEnabled,usbBuffer,numWords,numBytesPerBlock)
 end
 
-const USB_BUFFER_SIZE = 2400000
-const RHYTHM_BOARD_ID = 500
-const MAX_NUM_DATA_STREAMS = 8
-const FIFO_CAPACITY_WORDS = 67108864
 const SAMPLES_PER_DATA_BLOCK = 600
-
-const WireInResetRun = 0x00
-const WireInMaxTimeStepLsb = 0x01
-const WireInMaxTimeStepMsb = 0x02
-const WireInDataFreqPll = 0x03
-const WireInMisoDelay = 0x04
-const WireInCmdRamAddr = 0x05
-const WireInCmdRamBank = 0x06
-const WireInCmdRamData = 0x07
-const WireInAuxCmdBank1 = 0x08
-const WireInAuxCmdBank2 = 0x09
-const WireInAuxCmdBank3 = 0x0a
-const WireInAuxCmdLength1 = 0x0b
-const WireInAuxCmdLength2 = 0x0c
-const WireInAuxCmdLength3 = 0x0d
-const WireInAuxCmdLoop1 = 0x0e
-const WireInAuxCmdLoop2 = 0x0f
-const WireInAuxCmdLoop3 = 0x10
-const WireInLedDisplay = 0x11
-const WireInDataStreamSel1234 = 0x12
-const WireInDataStreamSel5678 = 0x13
-const WireInDataStreamEn = 0x14
-const WireInTtlOut = 0x15
-const WireInDacSource1 = 0x16
-const WireInDacSource2 = 0x17
-const WireInDacSource3 = 0x18
-const WireInDacSource4 = 0x19
-const WireInDacSource5 = 0x1a
-const WireInDacSource6 = 0x1b
-const WireInDacSource7 = 0x1c
-const WireInDacSource8 = 0x1d
-const WireInDacManual = 0x1e
-const WireInMultiUse = 0x1f
-
-const TrigInDcmProg = 0x40
-const TrigInSpiStart = 0x41
-const TrigInRamWrite = 0x42
-const TrigInDacThresh = 0x43
-const TrigInDacHpf = 0x44
-const TrigInExtFastSettle = 0x45
-const TrigInExtDigOut = 0x46
-
-const WireOutNumWordsLsb = 0x20
-const WireOutNumWordsMsb = 0x21
-const WireOutSpiRunning = 0x22
-const WireOutTtlIn = 0x23
-const WireOutDataClkLocked = 0x24
-const WireOutBoardMode = 0x25
-const WireOutBoardId = 0x3e
-const WireOutBoardVersion = 0x3f
-
-const PipeOutData = 0xa0
-
-#For 32 channel amps
-const PortA1 = 0
-const PortA2 = 1
-const PortB1 = 2
-const PortB2 = 3
-const PortC1 = 4
-const PortC2 = 5
-const PortD1 = 6
-const PortD2 = 7
-
-#For 64 channel amps
-const PortA1Ddr = 8
-const PortA2Ddr = 9
-const PortB1Ddr = 10
-const PortB2Ddr = 11
-const PortC1Ddr = 12
-const PortC2Ddr = 13
-const PortD1Ddr = 14
-const PortD2Ddr = 15
-
 const y = ccall((:okFrontPanel_Construct, mylib), Ptr{Void}, ())
 
 function init_board(lib::ASCIIString,bit::ASCIIString,)
@@ -127,6 +57,13 @@ function init_board(lib::ASCIIString,bit::ASCIIString,)
 
     #For 64 channel need two data streams, and data will come in 
     #on the rising AND falling edges of SCLK
+
+    for i=1:length(rhd.amps)
+
+        if typeof(rhd.amps
+        
+    end
+
     enableDataStream(rhd,1, true)
     setDataSource(rhd,0, 0) #port A MISO1
     setDataSource(rhd,1, 8) #port A MISO1 DDR
@@ -300,6 +237,8 @@ function initialize_board(rhd::RHD2000)
     setExternalDigOutChannel(rhd,"PortB", 0)
     setExternalDigOutChannel(rhd,"PortC", 0)
     setExternalDigOutChannel(rhd,"PortD", 0)
+
+    nothing
        
 end
 
@@ -309,6 +248,8 @@ function resetBoard()
     UpdateWireIns(rhd)
     SetWireInValue(rhd,WireInResetRun, 0x00, 0x01)
     UpdateWireIns(rhd)
+
+    nothing
     
 end
 
@@ -383,6 +324,8 @@ function setSampleRate(rhd::RHD2000,newSampleRate::Int64)
     #Wait for DataClkLocked = 1 before allowing data acquisition to continue
     while (isDataClockLocked(rhd) == false)
     end
+
+    nothing
                  
 end
 
@@ -423,6 +366,8 @@ function uploadCommandList(rhd::RHD2000,commandList, auxCommandSlot, bank)
         end
         
     end
+
+    nothing
     
 end
 
@@ -450,6 +395,8 @@ function selectAuxCommandBank(rhd::RHD2000,port, commandslot, bank)
     end
 
     UpdateWireIns(rhd)
+
+    nothing
 
 end
 
