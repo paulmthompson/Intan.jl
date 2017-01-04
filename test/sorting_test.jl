004fabe82a6d94a3fc9e2dd04bd48513b21df6a5
Paul Thompson <pmt10@case.edu>
2017-01-03 19:56:27 -0500

    added more sorting tests

--- test/sorting_test.jl
+++ test/sorting_test.jl
@@ -49,4 +49,66 @@ facts() do
 
 end
 
+#Delete Unit
+Intan.b1_cb_template(handles.run.handle,(handles,myrhd))
+
+facts() do
+
+	@fact handles.total_clus[1] --> 0
+	@fact handles.total_clus[2] --> 0
+end
+
+sleep(1.0)
+
+#Add Unit
+Intan.b2_cb_template(handles.run.handle,(handles,myrhd))
+
+press=Gtk.GdkEventButton(Gtk.GdkEventType.BUTTON_PRESS, Gtk.gdk_window(handles.c2),Int8(0),UInt32(0),142.0,252.0,convert(Ptr{Float64},C_NULL),UInt32(0),UInt32(1),C_NULL,0.0,0.0)
+signal_emit(handles.c2,"button-press-event",Bool,press)
+
+press=Gtk.GdkEventButton(Gtk.GdkEventType.BUTTON_PRESS, Gtk.gdk_window(handles.c2),Int8(0),UInt32(0),110.0,253.0,convert(Ptr{Float64},C_NULL),UInt32(0),UInt32(1),C_NULL,0.0,0.0)
+signal_emit(handles.c2,"button-release-event",Bool,press)
+
+sleep(1.0)
+
+#Add Second Unit
+Intan.b2_cb_template(handles.run.handle,(handles,myrhd))
+
+press=Gtk.GdkEventButton(Gtk.GdkEventType.BUTTON_PRESS, Gtk.gdk_window(handles.c2),Int8(0),UInt32(0),186.0,287.0,convert(Ptr{Float64},C_NULL),UInt32(0),UInt32(1),C_NULL,0.0,0.0)
+signal_emit(handles.c2,"button-press-event",Bool,press)
+
+press=Gtk.GdkEventButton(Gtk.GdkEventType.BUTTON_PRESS, Gtk.gdk_window(handles.c2),Int8(0),UInt32(0),156.0,293.0,convert(Ptr{Float64},C_NULL),UInt32(0),UInt32(1),C_NULL,0.0,0.0)
+signal_emit(handles.c2,"button-release-event",Bool,press)
+
+sleep(1.0)
+
+facts() do
+
+	@fact handles.total_clus[1] --> 2
+	@fact handles.total_clus[2] --> 0
+
+end
+
+#Change second unit
+
+press=Gtk.GdkEventButton(Gtk.GdkEventType.BUTTON_PRESS, Gtk.gdk_window(handles.c2),Int8(0),UInt32(0),186.0,287.0,convert(Ptr{Float64},C_NULL),UInt32(0),UInt32(1),C_NULL,0.0,0.0)
+signal_emit(handles.c2,"button-press-event",Bool,press)
+
+press=Gtk.GdkEventButton(Gtk.GdkEventType.BUTTON_PRESS, Gtk.gdk_window(handles.c2),Int8(0),UInt32(0),156.0,293.0,convert(Ptr{Float64},C_NULL),UInt32(0),UInt32(1),C_NULL,0.0,0.0)
+signal_emit(handles.c2,"button-release-event",Bool,press)
+
+sleep(1.0)
+
+facts() do
+
+	@fact handles.total_clus[1] --> 2
+	@fact handles.total_clus[2] --> 0
+
+end
+
+#Show Templates
+Intan.b4_cb_template(handles.run.handle,(handles,myrhd))
+
+sleep(1.0)
+
 end
