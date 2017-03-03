9e7f2e754ae980dac1f75b7e6b111b1148e6752d
Paul Thompson <pmt10@case.edu>
2017-03-03 16:08:02 -0500

    offline debugging can use multiple cores

--- src/offline.jl
+++ src/offline.jl
@@ -22,20 +22,43 @@ function readDataBlocks(rhd::RHD2000)
     #Filter
     #applyFilter(rhd)
 
-    #Sort
-    applySorting(rhd)
+    if typeof(rhd.fpga)==DArray{Intan.FPGA,1,Array{Intan.FPGA,1}}
+        if rhd.cal<3
+            offline_cal(rhd,rhd.s,rhd.v,rhd.buf,rhd.nums)
+        else
+            offline_sort(rhd,rhd.s,rhd.v,rhd.buf,rhd.nums)
+        end
+    else
+        applySorting(rhd)
+    end
     true
+end
+
+function offline_cal(rhd,s,v,buf,nums)
+    @sync for p in procs(rhd.fpga)
+        @spawnat p begin
+            cal!(localpart(s),v,buf,nums)
+        end 
+    end
+end
+
+function offline_sort(rhd,s,v,buf,nums)
+    @sync for p in procs(rhd.fpga)
+        @spawnat p begin
+            onlinesort!(localpart(s),v,buf,nums)
+        end 
+    end
 end
 
 function fillFromOffline!(rhd::RHD2000)
 
-    toff=rhd.time[end,rhd.fpga[1].id]
+    toff=rhd.time[end,1]
     for j=1:SAMPLES_PER_DATA_BLOCK
         for i=1:size(rhd.v,2)  
             rhd.v[j,i]=round(Int16,rhd.debug.data[rhd.debug.ind])
         end
         rhd.debug.ind+=1
-        rhd.time[j,rhd.fpga[1].id]=toff+j
+        rhd.time[j,1]=toff+j
     end
 
     if rhd.debug.ind>=rhd.debug.maxind
