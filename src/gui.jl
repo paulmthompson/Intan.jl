8acaac58eff513f56152e8cdc6cc1c819019d737
Paul Thompson <pmt10@case.edu>
2017-04-10 13:12:17 -0400

    actual values are loaded into parameter table when it is opened

--- src/gui.jl
+++ src/gui.jl
@@ -746,14 +746,6 @@ function table_en_cb(widget::Ptr,path,user_data::Tuple{Gui_Handles,RHD2000})
 
     han,rhd = user_data
 
-    #selmodel = Gtk.GAccessor.selection(han.table_widgets.tv)
-
-    #iter=Gtk.selected(selmodel)
-
-    #val=getindex(han.table_widgets.list,1,5)
-
-    #println(val)
-
     num=parse(Int64,unsafe_string(path))+1
 
     val=getindex(han.table_widgets.list,num,5)
@@ -1477,6 +1469,15 @@ end
 function table_cb(widget::Ptr, user_data::Tuple{Gui_Handles,RHD2000})
 
     han, rhd = user_data
+
+    for i=1:size(rhd.v,2)
+
+        setindex!(han.table_widgets.list,round(Int64,han.scale[i,1]*-1000),i,2)
+        setindex!(han.table_widgets.list,han.thres,i,3)
+        setindex!(han.table_widgets.list,rhd.refs[i],i,4)
+        setindex!(han.table_widgets.list,han.enabled[i],i,5)
+
+    end
 
     visible(han.table_widgets.win,true)
     nothing
