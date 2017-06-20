0def079ab86f0b9908f922168bb53508a705abeb
Paul Thompson <pmt10@case.edu>
2017-06-20 09:12:33 -0700

    separate reference channel file

--- src/gui.jl
+++ src/gui.jl
@@ -1801,24 +1801,6 @@ function load_backup_cb{R<:RHD2000,S<:Sorting}(widget::Ptr,user_data::Tuple{Gui_
     nothing
 end
 
-function update_ref(rhd::RHD2000,han::Gui_Handles)
-
-    selmodel_l=Gtk.GAccessor.selection(han.ref_tv1)
-    selmodel_r=Gtk.GAccessor.selection(han.ref_tv2)
-    
-    for i=1:size(rhd.v,2)
-
-        myref=rhd.refs[i]
-
-        if myref!=0
-
-            select!(selmodel_l,Gtk.iter_from_index(han.ref_list1,myref))
-            select!(selmodel_r,Gtk.iter_from_index(han.ref_list2,i))         
-        end
-    end
-    nothing
-end
-
 #=
 Right Canvas Callbacks
 =#
@@ -1963,52 +1945,6 @@ function get_multi_dims(han::Gui_Handles,n_col,n_row,num_chan,spike)
     x=div(rem(spike-1,num_chan),n_row)+1
     
     (xbounds[x],xbounds[x+1],ybounds[y],ybounds[y+1])
-end
-
-
-
-function ref_b1_cb(widget::Ptr, user_data::Tuple{Gui_Handles})
-
-    han, = user_data
-    selmodel=Gtk.GAccessor.selection(han.ref_tv1)
-    selectall!(selmodel)
-
-    nothing
-end
-
-function ref_b2_cb(widget::Ptr, user_data::Tuple{Gui_Handles})
-
-    han, = user_data
-    selmodel=Gtk.GAccessor.selection(han.ref_tv2)
-    selectall!(selmodel)
-
-    nothing
-end
-
-function ref_b3_cb(widget::Ptr, user_data::Tuple{Gui_Handles,RHD2000})
-
-    han, rhd = user_data
-
-    #Left
-    selmodel=Gtk.GAccessor.selection(han.ref_tv1)
-    myref=parse(Int64,Gtk.get_string_from_iter(TreeModel(han.ref_list1), Gtk.selected(selmodel)))+1
-    
-    #Right
-    for i=1:size(rhd.v,2)
-        if is_selected(han.ref_list2,han.ref_tv2,i-1)
-            if i==myref
-                rhd.refs[i]=0
-            else
-                rhd.refs[i]=myref
-            end
-        end
-    end
-
-    f=open(string(rhd.save.backup,"ref.bin"),"w")
-    write(f,rhd.refs)
-    close(f)
-
-    nothing
 end
 
 function c3_press_win(widget::Ptr,param_tuple,user_data::Tuple{Gui_Handles})
---
+++ src/gui/reference_channel.jl
@@ -0,0 +1,66 @@
+
+function update_ref(rhd::RHD2000,han::Gui_Handles)
+
+    selmodel_l=Gtk.GAccessor.selection(han.ref_tv1)
+    selmodel_r=Gtk.GAccessor.selection(han.ref_tv2)
+    
+    for i=1:size(rhd.v,2)
+
+        myref=rhd.refs[i]
+
+        if myref!=0
+
+            select!(selmodel_l,Gtk.iter_from_index(han.ref_list1,myref))
+            select!(selmodel_r,Gtk.iter_from_index(han.ref_list2,i))         
+        end
+    end
+    nothing
+end
+
+#=
+Callbacks for reference channel selection
+=#
+
+function ref_b1_cb(widget::Ptr, user_data::Tuple{Gui_Handles})
+
+    han, = user_data
+    selmodel=Gtk.GAccessor.selection(han.ref_tv1)
+    selectall!(selmodel)
+
+    nothing
+end
+
+function ref_b2_cb(widget::Ptr, user_data::Tuple{Gui_Handles})
+
+    han, = user_data
+    selmodel=Gtk.GAccessor.selection(han.ref_tv2)
+    selectall!(selmodel)
+
+    nothing
+end
+
+function ref_b3_cb(widget::Ptr, user_data::Tuple{Gui_Handles,RHD2000})
+
+    han, rhd = user_data
+
+    #Left
+    selmodel=Gtk.GAccessor.selection(han.ref_tv1)
+    myref=parse(Int64,Gtk.get_string_from_iter(TreeModel(han.ref_list1), Gtk.selected(selmodel)))+1
+    
+    #Right
+    for i=1:size(rhd.v,2)
+        if is_selected(han.ref_list2,han.ref_tv2,i-1)
+            if i==myref
+                rhd.refs[i]=0
+            else
+                rhd.refs[i]=myref
+            end
+        end
+    end
+
+    f=open(string(rhd.save.backup,"ref.bin"),"w")
+    write(f,rhd.refs)
+    close(f)
+
+    nothing
+end
