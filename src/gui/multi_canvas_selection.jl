08fcb7a11a4a0fb1833c7cac752831c43c180b74
Paul Thompson <pmt10@case.edu>
2017-06-20 08:44:05 -0700

    separate soft scope file

---
+++ src/gui/multi_canvas_selection.jl
@@ -0,0 +1,85 @@
+
+
+#=
+Buttons on right side determine what is displayed on canvas1
+=#
+
+function rb1_cb(widgetptr::Ptr,user_data::Tuple{Gui_Handles,Int64})
+
+    han, event_id = user_data
+
+    if han.c_right_top != event_id
+        han.c_right_top=event_id
+
+        if event_id==3
+            if han.c_right_bottom != 5
+                setproperty!(han.rb2[5],:active,true)
+            end
+        elseif event_id==4
+            if han.c_right_bottom != 6
+                setproperty!(han.rb2[6],:active,true)
+            end
+        else
+            if (han.c_right_bottom == 5)|(han.c_right_bottom == 6)
+                setproperty!(han.rb2[8],:active,true)
+            end
+        end
+    end
+
+    if han.c_right_top == 1
+	set_slider(han,16)
+    elseif han.c_right_top == 2
+	set_slider(han,32)
+    elseif (han.c_right_top == 3)|(han.c_right_top == 4)
+	set_slider(han,64)
+    end
+    
+    clear_c(han)
+    nothing
+end
+
+function rb2_cb(widgetptr::Ptr,user_data::Tuple{Gui_Handles,Int64})
+
+    han, event_id = user_data
+
+    if han.c_right_bottom != event_id
+        han.c_right_bottom=event_id
+
+        if event_id==5
+            if han.c_right_top!=3
+                setproperty!(han.rb1[3],:active,true)
+            end
+        elseif event_id==6
+            if han.c_right_top != 4
+                setproperty!(han.rb1[4],:active,true)
+            end
+        elseif event_id == 7
+            ctx=getgc(han.c)
+            han.c.back.ptr = CairoRGBSurface(width(ctx),height(ctx)).ptr
+            han.c.backcc = CairoContext(han.c.back)
+        else
+            if (han.c_right_top == 3)|(han.c_right_top == 4)
+                setproperty!(han.rb1[1],:active,true)
+            end
+        end
+    end
+
+    clear_c(han)
+    nothing
+end
+
+function set_slider(han::Gui_Handles,chan_num::Int64)
+    han.num16=div(han.spike-1,chan_num)+1
+    han.num=rem(han.spike-1,chan_num)+1
+    han.chan_per_display=chan_num
+    if han.num16*chan_num>length(han.enabled)
+        setproperty!(han.adj2,:upper,han.num16*chan_num-length(han.enabled))
+        setproperty!(han.adj,:upper,div(length(han.enabled),chan_num)+1)
+    else
+        setproperty!(han.adj2,:upper,chan_num)
+        setproperty!(han.adj,:upper,div(length(han.enabled),chan_num))
+    end
+    setproperty!(han.adj2, :value, han.num)
+    setproperty!(han.adj, :value, han.num16)  
+    nothing
+end
---
+++ src/gui/soft_scope.jl
@@ -0,0 +1,169 @@
+
+function scope_popup_v_cb(widgetptr::Ptr,user_data::Tuple{Gui_Handles,Int64})
+
+    han, event_id = user_data
+    
+    if event_id==0
+        han.soft.v_div=1.0
+    elseif event_id==1
+        han.soft.v_div=50.0
+    elseif event_id==2
+        han.soft.v_div=100.0
+    elseif event_id==3
+        han.soft.v_div=200.0
+    elseif event_id==4
+        han.soft.v_div=500.0
+    end
+
+    han.soft.v_div /= 1000.0
+
+    nothing
+end
+
+function scope_popup_t_cb(widgetptr::Ptr,user_data::Tuple{Gui_Handles,Int64})
+
+    han, event_id = user_data
+    
+    if event_id==0
+        han.soft.t_div=1.0
+    elseif event_id==1
+        han.soft.t_div=2.0
+    elseif event_id==2
+        han.soft.t_div=3.0
+    elseif event_id==3
+        han.soft.t_div=4.0
+    elseif event_id==4
+        han.soft.t_div=5.0
+    end
+
+    nothing
+end
+
+function scope_popup_thres_cb(w::Ptr,user_data::Tuple{Gui_Handles,Int64})
+    
+    han, event_id = user_data
+
+    if event_id==0
+        han.soft.thres_on=true
+    else
+        han.soft.thres_on=false
+    end
+
+    nothing
+end
+
+function draw_scope(rhd::RHD2000,han::Gui_Handles)
+
+    ctx=getgc(han.c)
+    myheight=height(ctx)
+
+    if han.soft.draws>9
+
+        startind=512*(han.soft.draws-1)+1
+        for i=1:512
+            han.soft.v[startind]=rhd.v[i,han.spike]
+            startind+=1
+        end
+        
+        #Paint over old line
+        move_to(ctx,1.0,han.soft.last[1])
+        for i=2:499
+            line_to(ctx,i,han.soft.last[i])
+        end
+
+        #Paint over old asterisks
+        move_to(ctx,1.0,myheight-4.0)
+        line_to(ctx,512.0,myheight-4.0)
+        move_to(ctx,1.0,myheight-7.0)
+        line_to(ctx,512.0,myheight-7.0)
+        
+        set_source_rgb(ctx,0.0,0.0,0.0)
+        set_line_width(ctx,4.0)
+        stroke(ctx)  
+  
+        s=han.soft.v_div*-1
+        
+        #Draw voltage trace from desired channel
+        move_to(ctx,1.0,myheight-150.0+han.soft.v[1]*s)
+        han.soft.last[1]=myheight-150.0+han.soft.v[1]*s
+
+        t_iter=floor(Int64,han.soft.t_div)
+
+        spike_ind=1
+        
+        scope_ind=1+t_iter
+        for i=2:512
+            y=myheight-150.0+han.soft.v[scope_ind]*s
+            line_to(ctx,i,y)
+            han.soft.last[i]=y
+            
+            if scope_ind>han.soft.spikes[spike_ind]
+                han.soft.prev_spikes[spike_ind]=i
+                spike_ind+=1
+            end
+
+            scope_ind+=t_iter
+        end
+        set_source_rgb(ctx,1.0,1.0,1.0)
+        set_line_width(ctx,0.5)      
+        stroke(ctx)
+
+        #reset spikes
+        for i=1:(spike_ind-1)
+            move_to(ctx,han.soft.prev_spikes[i],myheight)
+            show_text(ctx,"*")
+        end
+        han.soft.prev_num_spikes=spike_ind-1
+        han.soft.num_spikes=0
+        
+        #draw threshold
+
+        if han.soft.thres_on
+            plot_thres_scope(han,rhd,ctx)
+        end
+
+        han.soft.draws=1
+    else
+        #copy voltage
+        startind=512*(han.soft.draws-1)+1
+        for i=1:512
+            han.soft.v[startind]=rhd.v[i,han.spike]
+            startind+=1
+        end
+
+        #get spikes
+        for g=1:rhd.nums[han.spike]
+            han.soft.num_spikes+=1
+            han.soft.spikes[han.soft.num_spikes]=rhd.buf[g,han.spike].inds.start+512*(han.soft.draws-1)+1
+        end
+  
+        han.soft.draws+=1
+    end
+
+    nothing
+end
+
+function plot_thres_scope(han,rhd,ctx)
+
+    myheight=height(ctx)
+    
+    thres=han.thres*han.soft.v_div
+
+    move_to(ctx,1,myheight-150-thres+2)
+    line_to(ctx,500,myheight-150-thres+2)
+
+    move_to(ctx,1,myheight-150-thres-2)
+    line_to(ctx,500,myheight-150-thres-2)
+
+    set_line_width(ctx,5.0)
+    set_source_rgb(ctx,0.0,0.0,0.0)
+    stroke(ctx)
+
+    move_to(ctx,1,myheight-150-thres)
+    line_to(ctx,500,myheight-150-thres)
+    set_line_width(ctx,1.0)
+    set_source_rgb(ctx,1.0,1.0,1.0)
+    stroke(ctx)
+    
+    nothing
+end
