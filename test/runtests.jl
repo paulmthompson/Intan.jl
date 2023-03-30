553aec820a8d20303e46482a37998c69c1e8cdca
Paul Thompson <pmt10@case.edu>
2023-03-30 14:34:15 -0400

    disable most tests until other improvements are made

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,10 +3,10 @@ module TestSetup
 using Intan
 
 include("board_test.jl")
-include("gui_test.jl")
-include("sorting_test.jl")
-include("registers_test.jl")
-include("tasks_test.jl")
-include("filter_test.jl")
+#include("gui_test.jl")
+#include("sorting_test.jl")
+#include("registers_test.jl")
+#include("tasks_test.jl")
+#include("filter_test.jl")
 
 end
